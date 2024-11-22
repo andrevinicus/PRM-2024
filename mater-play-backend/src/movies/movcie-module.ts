a7993a7c3851fda152ae6e754ede3ddb9d80d1f6
André <andre.gorlin@gmail.com>
2024-11-21 22:07:53 -0300

    Aula 21/11/2024

--- mater-play-backend/src/movies/movcie-module.ts
+++ mater-play-backend/src/movies/movcie-module.ts
@@ -5,10 +5,11 @@ import { Module } from "@nestjs/common";
 import { Movie } from "./movie-entity";
 import { Category } from "src/categories/category-entity";
 import { Genero } from "src/genero/genero-entity";
+import { SupabaseModule } from "src/@libs/subpasta/supabase.module";
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Movie, Category, Genero])],
+    imports: [TypeOrmModule.forFeature([Movie, Category, Genero]), SupabaseModule],
     controllers: [MovieController],
     providers: [MovieService],
 })
